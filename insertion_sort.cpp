b4afded7d3d77c5dbfeaccbb136da4953d11d399
Rronny01 <72240947+Rronny01@users.noreply.github.com>
2021-10-01 09:54:05 +0530

    insertion sort of numbers

---
+++ insertion_sort.cpp
@@ -0,0 +1,28 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ int x[5],i,j,k,d,tmp;
+ clrscr();
+ cout<<"Enter no. for sorting=";
+  for(i=0;i<5;i++)
+    {
+     cin>>d>>x[i];
+    }
+ for(i=0;i<5;i++)
+   {
+     tmp=x[i];
+       for(j=i-1;j>=0&&tmp<x;j--)
+	{
+	  x[j+1]=x[j];
+	  x[j]=tmp;
+	}
+    }
+
+      cout<<"sorted order is=";
+	    for(j=0;j<5;j++)
+	     {
+	      cout<<d<<"\n"<<x[j];
+	     }
+ getch();
+}
