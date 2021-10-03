984787b87bb84a74b1f335acf7ce0b0520b20520
itssankarraj <56398367+itssankarraj@users.noreply.github.com>
2021-10-03 17:33:15 +0530

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -25,12 +25,12 @@ print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter your choice(1/2/3/4): ")
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num1 = float(input("Enter first no: "))
+        num2 = float(input("Enter second no: "))
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
@@ -52,3 +52,5 @@ while True:
     
     else:
         print("Invalid Input")
+}
+}
